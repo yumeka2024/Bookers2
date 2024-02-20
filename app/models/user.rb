3d2298b96fd8c77bce1dc390c47fb412e0a18075
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-02-20 06:48:48 +0000

    [Add]プロフィール画像

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  
+
+  #プロフィール画像カラムの作成
+  has_one_attached :profile_image
+
+  #プロフィール画像を表示させるメソッド
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
