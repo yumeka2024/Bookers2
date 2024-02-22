11531d140112d98bc47460d02f29acf00429eb45
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-02-22 20:06:22 +0000

    [Add]RSpec

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,10 @@
 class Book < ApplicationRecord
-  
+
   #1(user):N
-  belongs_to :user
-  
+  belongs_to :user, optional: true
+
   #バリデーション
   validates :title, presence:true
   validates :body, presence:true, length: { maximum: 200 }
-  
+
 end
